593b946afffb4a5ddd321a177f9a046a00e49e03
Simon Sickle <simon@simonsickle.com>
2016-01-29 17:21:27 -0800

    flounder: prepare for nightly builds
    Change-Id: I7c6585778571e5c0179842afa09c515370208a01
    Signed-off-by: Simon Sickle <simon@simonsickle.com>

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -1,5 +1,5 @@
 # Kernel settings
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/a$
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/htc/flounder
 TARGET_KERNEL_CONFIG := flounder_defconfig
