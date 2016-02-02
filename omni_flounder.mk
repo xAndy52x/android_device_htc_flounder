56518f59cbfb5d8f23b926fde045f87fc635d163
Simon Sickle <simon@simonsickle.com>
2016-02-01 18:30:02 -0800

    flounder: february security update
    Change-Id: I50774407d5724f984c00f5892133880bbe3e7d8b
    Signed-off-by: Simon Sickle <simon@simonsickle.com>

--- omni_flounder.mk
+++ omni_flounder.mk
@@ -39,9 +39,9 @@ PRODUCT_MODEL := Nexus 9
 # Fix for vendor / system prop mismatch
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29S/2489379:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29S 2489379 release-keys" \
-    BUILD_ID=MMB29S
+    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29R/2482564:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29R 2482564 release-keys" \
+    BUILD_ID=MMB29R
 
 # SELinux
 PRODUCT_PROPERTY_OVERRIDES += \
