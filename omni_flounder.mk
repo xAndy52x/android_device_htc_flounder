74ea5f5f05a33faa88df3059d049b2cd6930ea5f
Hardslog <hardslog@gmail.com>
2017-02-01 17:48:42 -0500

    change the order of BoardConfigOmni
    Needs to come after vendor/omni/config/common_tablet.mk so that it will
    pick up the dexpreopt false flag properly
    
    Change-Id: I186b26f04ccb920328ad7f7723636faf5e28ff84

--- omni_flounder.mk
+++ omni_flounder.mk
@@ -17,11 +17,11 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
-# Omni specific board config
-include device/htc/flounder/BoardConfigOmni.mk
-
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common_tablet.mk)
+
+# Omni specific board config
+include device/htc/flounder/BoardConfigOmni.mk
 
 # Inherit device configuration
 $(call inherit-product, device/htc/flounder/aosp_flounder64.mk)
